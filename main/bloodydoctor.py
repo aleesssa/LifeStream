6d7ab2b539c993ec7abb0b1a2eca7c77bff17e00
aleesssa <aleessab@gmail.com>
2025-01-29 20:23:19 +0800

    added requirements.txt

--- main/bloodydoctor.py
+++ main/bloodydoctor.py
@@ -1,12 +1,61 @@
+hearts = 5
+xp = 0
+level = 0
+  
+
+def patientsInfo(level):
+    patients = [
+        {
+            'name' : 'Moana binti Drake',
+            'age' : 12,
+            'condition' : 'Anaemic',
+            'blood_type' : 'A',
+            'allergy' : 'peanut butter'
+        },
+        {
+            'name' : 'Jaehyun bin Jamal',
+            'age' : 27,
+            'condition' : 'Iron deficiency',
+            'blood_type' : 'B',
+            'allergy' : 'Roses'
+        },        
+        {
+            'name' : 'Suka binti Dessert',
+            'age' : 45,
+            'condition' : 'Diabetes',
+            'blood_type' : 'B',
+            'allergy' : 'None'
+        },        
+        {
+            'name' : 'Baby Boss',
+            'age' : 1,
+            'condition' : 'Fell from bed',
+            'blood_type' : 'O',
+            'allergy' : 'Eggs'
+        },        
+        {
+            'name' : 'Moana binti Drake',
+            'age' : 12,
+            'condition' : 'Anaemic',
+            'blood_type' : 'B',
+            'allergy' : 'peanut butter'
+        }
+        ]
+    
+    return patients[level]
+
+# Patient(name, age, condition, blood_type, allergy)
+# Quiz(question, answer, hint, exp) 
+
+# INFO
 print("""
-# ********************************************************************
-# Program: bloody-doctor.py
+# ***********************************************************************************************************************************************************************
+# Program: bloodydoctor.py
 # Course: CSP1114 PROBLEM SOLVING AND PROGRAM DESIGN
 # Lecture / Lab Section: TC4L
 # Trimester: 2430
-# Names: ALEESSA BATRISYIA BINTI AZWAN | ALYA | NUR DAMIA BATRISYIA | QAISARAH
+# Names: ALEESSA BATRISYIA BINTI AZWAN | NUR ALYA IMAN BINTI MOHD PAZLI YUSOF | NUR DAMIA' BATRISYIA BINTI MOHAMMAD DENEE ROSDI | QAISARAH BINTI SHAMSUL AZRAN
 # IDs: 
-# Emails:
-# **********************************************************************        
+# Emails: 
+# ************************************************************************************************************************************************************************  
 """)
-
